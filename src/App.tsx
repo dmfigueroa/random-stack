7a26325cc62eff386ece5908c3d74da70f0a9445
David Figueroa <davidfigueroa0327@gmail.com>
2024-01-15 22:38:52 -0500

    add routing and admin layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/admin",
+    lazy: () => import("./layouts/admin-layout"),
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider>
       <ModeToggle />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
