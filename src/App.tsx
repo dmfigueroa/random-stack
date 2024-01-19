f6dc0825213ab2f41eeb4b3c98b0a32b6774b47e
David Figueroa <davidfigueroa0327@gmail.com>
2024-01-19 17:37:58 -0500

    added categories route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
 
 const router = createBrowserRouter([
+  { path: "/", element: <div>Home</div> },
   {
-    path: "/",
-    element: <div>Home</div>,
-  },
-  {
-    path: "/admin",
-    lazy: () => import("./layouts/admin-layout"),
+    path: "admin",
+    loader: () =>
+      import.meta.env.VITE_ADDING_MODE !== "true" ? redirect("/") : null,
+    children: [
+      { index: true, element: <Navigate to="/admin/categories" replace /> },
+      { path: "categories", lazy: () => import("./pages/admin/categories") },
+    ],
   },
 ]);
 
 function App() {
   return (
     <ThemeProvider>
-      <ModeToggle />
+      <header className="flex items-center justify-center py-4 px-8">
+        <h1 className="text-4xl font-bold tracking-tight mx-auto">
+          Random Stack
+        </h1>
+        <ModeToggle />
+      </header>
       <RouterProvider router={router} />
     </ThemeProvider>
   );
